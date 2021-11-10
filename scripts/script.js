ab2829dc917e03e8d0ef98ddea2279441f131dc6
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2021-11-10 08:39:47 +0100

    Fixed image display issues

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,23 @@ switch (currentTheme) {
     case "light-theme":
         document.body.classList.toggle(currentTheme);
         theme_icon.src = "../images/icon-moon.svg";
-        document.body.style.backgroundImage = "url('../images/bg-desktop-light.jpg')";
+        document.body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
         break;
     default:
         document.body.classList.toggle(currentTheme);
         theme_icon.src = "../images/icon-sun.svg";
-        document.body.style.backgroundImage = "url('../images/bg-desktop-dark.jpg')";
+        document.body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
         break;
 }
 switchTheme = () => {
     document.body.classList.toggle("light-theme");
     if (document.body.classList.contains("light-theme")) {
         theme_icon.src = "../images/icon-moon.svg";
-        document.body.style.backgroundImage = "url('../images/bg-desktop-light.jpg')";
+        document.body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
         localStorage.setItem('theme', 'light-theme');
     } else {
         theme_icon.src = "../images/icon-sun.svg";
-        document.body.style.backgroundImage = "url('../images/bg-desktop-dark.jpg')";
+        document.body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
         localStorage.setItem('theme', 'dark-theme');
     }
 }
