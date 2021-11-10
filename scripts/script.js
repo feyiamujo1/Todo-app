d1c496cd6cc648668299497913f9ea72562689b8
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2021-11-10 13:25:41 +0100

    Making the theme toggle button display correctly

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,23 @@ var theme_icon = document.getElementById("theme_toggle_icon");
 switch (currentTheme) {
     case "light-theme":
         document.body.classList.toggle(currentTheme);
-        theme_icon.src = "../images/icon-moon.svg";
+        theme_icon.src = "./images/icon-moon.svg";
         document.body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
         break;
     default:
         document.body.classList.toggle(currentTheme);
-        theme_icon.src = "../images/icon-sun.svg";
+        theme_icon.src = "./images/icon-sun.svg";
         document.body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
         break;
 }
 switchTheme = () => {
     document.body.classList.toggle("light-theme");
     if (document.body.classList.contains("light-theme")) {
-        theme_icon.src = "../images/icon-moon.svg";
+        theme_icon.src = "./images/icon-moon.svg";
         document.body.style.backgroundImage = "url('./images/bg-desktop-light.jpg')";
         localStorage.setItem('theme', 'light-theme');
     } else {
-        theme_icon.src = "../images/icon-sun.svg";
+        theme_icon.src = "./images/icon-sun.svg";
         document.body.style.backgroundImage = "url('./images/bg-desktop-dark.jpg')";
         localStorage.setItem('theme', 'dark-theme');
     }
